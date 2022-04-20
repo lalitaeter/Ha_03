b504816d7b10da57283fe4f4ff0991783496c1a2
Dwennimmen <amini_l2001@yahoo.de>
2022-04-20 13:20:13 +0200

    einen Hut hinzugefügt

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -3,6 +3,6 @@ package idh.java;
 
 public class Main {
 	public static void main(String[] args) {
-    System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Aggregat und eine Powerbank und ein Erdbeermarmeladebrot mit Honig und Schokolade und einem Becher Kakao.");
+    System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Aggregat und eine Powerbank und ein Erdbeermarmeladebrot mit Honig und Schokolade und einem Becher Kakao und einen Hut.");
 	};
 }
