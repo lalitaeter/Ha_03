d29e1da863406815c6489c22392bb51e90bb0258
Luka <Luka@DESKTOP-74NA0L6.fritz.box>
2022-04-23 13:13:45 +0200

    added another item

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -3,5 +3,5 @@ package idh.java;
 
 public class Main {
 	public static void main(String[] args) {
-	System.out.println("Ich packe in meinen Koffer ein Netzteil fÃ¼r mein Telefon und ein Aggregat und eine Powerbank und ein Erdbeermarmeladebrot mit Honig und Schokolade und einem Becher Kakao und einen Hut und Sonnencreme und mein Handy und einen Baustrahler sowie ein Chalkbag, dann lege ich noch ein..warte mal...hey..w..was?..ist das!? Die Lichter kommen...Ahhhhrr!ðŸ›¸ï¸�ðŸ‘½ï¸�, dazu noch einen Ball und eine Luftmatratze und noch eine Wassermelone");	};
+	System.out.println("Ich packe in meinen Koffer ein Netzteil fÃ¼r mein Telefon und ein Aggregat und eine Powerbank und ein Erdbeermarmeladebrot mit Honig und Schokolade und einem Becher Kakao und einen Hut und Sonnencreme und mein Handy und einen Baustrahler sowie ein Chalkbag, dann lege ich noch ein..warte mal...hey..w..was?..ist das!? Die Lichter kommen...Ahhhhrr!ðŸ›¸ï¸�ðŸ‘½ï¸�, dazu noch einen Ball und eine Luftmatratze und noch eine Wassermelone, meinen 27-seitigen Beweis, dass die CIA hinter dem Abschuss von MH17 steckt,");	};
 }
