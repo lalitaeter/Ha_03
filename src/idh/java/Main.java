11ed60764c26b645e935fd26993ddc40fe33e10e
ltadsen <larissa45@live.de>
2023-04-19 12:54:23 +0200

    added String h

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -3,9 +3,9 @@ package idh.java;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Handy.");
+		String h = "und ein Handy";
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon" + h);
 	};
 }
