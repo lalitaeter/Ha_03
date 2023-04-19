8c278a6546b0510b672e480f64081d06a1ed6305
JohannonBlake <johannon.blade@Gmail.com>
2023-04-19 11:20:11 +0200

    first!

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, und einen Beutel voller Würfel ");
 	};
 }
