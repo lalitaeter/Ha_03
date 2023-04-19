7cd44198c02b2e72977ce3e8b9f20e6438f3e6ce
Celine <Celine@172.29.202.154>
2023-04-19 13:50:58 +0200

    Added the Kartenspiel

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Kartenspiel.");
 	};
 }
