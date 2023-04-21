7c1bdc69d3c2b713c01ba5f48bfb1b884c987f1d
floeliasrux <floeliasrux@rux-floelias>
2023-04-21 17:51:31 +0200

    Eis hinzugefügt

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -1,11 +1,10 @@
 package idh.java;
 
-
 public class Main {
 	public static void main(String[] args) {
-		
+
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Eis.");
 	};
 }
