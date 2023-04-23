960abf5f24c391fb13ff037e15697f475d8bf8f2
Celine <Celine@DESKTOP-N8FOK3G.fritz.box>
2023-04-23 22:12:40 +0200

    added Kartenspiel

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Kartenspiel.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite"
+				+ "und ein Handy.");
 	};
 }
