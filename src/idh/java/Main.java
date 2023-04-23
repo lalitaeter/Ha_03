7eeade14f80de74d597c852ecd271215a77b13a2
ltadsen <larissa45@live.de>
2023-04-23 21:42:05 +0200

    Handy hinzugefügt

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -3,9 +3,10 @@ package idh.java;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		String h = "und ein Handy";
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon" + h);
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite"
+				+ "und ein Handy.");
 	};
 }
