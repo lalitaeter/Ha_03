75404c1115bc4e85402372afd682ddc179773bbd
ltadsen <larissa45@live.de>
2023-04-23 21:09:21 +0200

    merge conflict solved

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		String h = "und ein Handy";
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon" + h);
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon");
 	};
 }
