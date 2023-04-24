570444032c652ca414b0b07c7fc3aa1533c3b1c3
Kbleser <97674016+Inglorious-Inge@users.noreply.github.com>
2023-04-24 02:41:25 +0200

    Update Main.java
    Towl added

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -7,6 +7,6 @@ public class Main {
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
 
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Faultier und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite und eine Sonnenbrille" + "und ein Handy und eine Kartoffelsuppe und ein Eis.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Faultier und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite und eine Sonnenbrille" + "und ein Handy und eine Kartoffelsuppe und ein Eis und ein Handtuch.");
 	};
 }
