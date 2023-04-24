fddcd663de369b9ab6195d350c5f7cb3c01c316a
WavyLotos <steph@LAPTOP-I3378U9E.fritz.box>
2023-04-24 11:38:05 +0200

    Sonnenbrille

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
 
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Apache attack helicopter und ein Faultier und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite und eine Sonnenbrille" + "und ein Handy und eine Kartoffelsuppe und ein Eis und ein Handtuch und ein Stabmixer und Prof. Dr. Øyvind Eide und einen Bassisten einer Baby-Katzen-Metalband,  und einen Beutel voller Würfel.");
->>>>>>> 8f585e286501020a1c42fe1d8853b01286f450b2
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Apache attack helicopter und ein Faultier und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite und eine Sonnenbrille" + "und ein Handy und eine Kartoffelsuppe und ein Eis und ein Handtuch und ein Stabmixer und Prof. Dr. Øyvind Eide und einen Bassisten einer Baby-Katzen-Metalband,  und einen Beutel voller Würfel." + "Und eine mega schöne Sonnenbrille");
+
 	};
 }
