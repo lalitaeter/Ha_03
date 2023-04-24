00e468f63c57e8d5ad33b45a5c053448070423ec
larsbieber <lbieber1@smail.uni-koeln.de>
2023-04-24 09:40:39 +0200

    Added Prof Eide to the bag

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und Prof. Dr. Øyvind Eide.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und Prof. Øyvind Eide.");
 	};
 }
